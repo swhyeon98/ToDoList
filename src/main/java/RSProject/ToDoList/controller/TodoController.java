1f3d32d6d497a9c88df3f7097c3fd4fb39687139
hyeonseog-won <smreo75@gmail.com>
2022-06-24 20:29:42 +0900

    Changes

--- src/main/java/RSProject/ToDoList/controller/TodoController.java
+++ src/main/java/RSProject/ToDoList/controller/TodoController.java
@@ -1,10 +1,28 @@
 package RSProject.ToDoList.controller;
 
+import RSProject.ToDoList.dto.ResponseDTO;
+import RSProject.ToDoList.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+    @Autowired
+    private TodoService service;
 
+    @GetMapping("/test")
+    public ResponseEntity<?> testTodo() {
+        String str = service.testService();
+        List<String> list = new ArrayList<>();
+        list.add(str);
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
+    }
 }
